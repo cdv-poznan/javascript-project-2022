a3f667379df51d8eee6f383e08617c622bdfb2a7
Adrian Juszczak <adrian@juszczak.io>
2022-02-13 11:12:45 +0100

    Chart.js
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('header').style.color = '#666';
+
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+
+  const config = {
+    type: 'line',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [
+        {
+          label: 'A',
+          data: [12, 10, 3, 5, 2, 3],
+          borderWidth: 4,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+
+  const chart = new Chart(context, config);
 });
