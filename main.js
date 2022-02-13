53e4d7af7c8481ab647cacdaf1a661d86dfa915e
Adrian Juszczak <adrian@juszczak.io>
2022-02-13 11:27:26 +0100

    Gold prices
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
+async function fetchGoldPrices(startDate, endDate) {
+  const res = await fetch(
+    `https://api.nbp.pl/api/cenyzlota/${startDate}/${endDate}/?format=json`
+  );
+  return await res.json();
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
   document.getElementById('header').style.color = '#666';
 
   const canvas = document.getElementById('canvas');
   const context = canvas.getContext('2d');
 
+  const goldPrices = await fetchGoldPrices('2022-01-01', '2022-02-13');
+
+  const dates = goldPrices.map(({data}) => data);
+  const prices = goldPrices.map(({cena}) => cena);
+
   const config = {
     type: 'line',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: dates,
       datasets: [
         {
-          label: 'A',
-          data: [12, 10, 3, 5, 2, 3],
+          label: 'Cena 1g złowa o próbie 1000',
+          data: prices,
           borderWidth: 4,
+          backgroundColor: ['rgba(255, 206, 86, 0.2)'],
+          borderColor: ['rgba(255, 206, 86, 1)'],
         },
       ],
     },
     options: {
       scales: {
         y: {
-          beginAtZero: true,
+          beginAtZero: false,
         },
       },
     },
