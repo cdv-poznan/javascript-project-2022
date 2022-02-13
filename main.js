1394892ff0271f53d14281ab043de0f8f531f0da
Adrian Juszczak <adrian@juszczak.io>
2022-02-13 14:56:20 +0100

    Weather
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- main.js
+++ main.js
@@ -154,9 +154,68 @@ async function displayNews() {
   }, 10000);
 }
 
+async function initGeoWeather() {
+  const geoDiv = document.getElementById('geo');
+  const weatherDiv = document.getElementById('weather');
+
+  /* Code	Description
+    0	Clear sky
+    1, 2, 3	Mainly clear, partly cloudy, and overcast
+    45, 48	Fog and depositing rime fog
+    51, 53, 55	Drizzle: Light, moderate, and dense intensity
+    56, 57	Freezing Drizzle: Light and dense intensity
+    61, 63, 65	Rain: Slight, moderate and heavy intensity
+    66, 67	Freezing Rain: Light and heavy intensity
+    71, 73, 75	Snow fall: Slight, moderate, and heavy intensity
+    77	Snow grains
+    80, 81, 82	Rain showers: Slight, moderate, and violent
+    85, 86	Snow showers slight and heavy
+    95 *	Thunderstorm: Slight or moderate
+    96, 99 *	Thunderstorm with slight and heavy hail */
+  const m = new Map();
+  m.set(0, 'Clear sky');
+  m.set(1, 'Mainly clear');
+  m.set(2, 'Partly cloudy');
+  m.set(3, 'Overcast');
+  m.set(45, 'Fod');
+  m.set(48, 'Depositing rime fog');
+
+  function getWeatherDescription(code) {
+    return m.get(code);
+  }
+
+  async function getWeather(lat, lon) {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&timeformat=unixtime`
+    const res = await fetch(url)
+    const {current_weather} = await res.json();
+    const {temperature, windspeed, winddirection, weathercode} = current_weather;
+
+    const tempDiv = document.createElement('div');
+    tempDiv.classList.add('temp');
+    tempDiv.innerText = `${temperature} °C`;
+    weatherDiv.appendChild(tempDiv);
+
+    const descriptionDiv = document.createElement('div');
+    descriptionDiv.classList.add('weather-description');
+    descriptionDiv.innerText = getWeatherDescription(weathercode);
+    weatherDiv.appendChild(descriptionDiv);
+  }
+
+  navigator.geolocation.getCurrentPosition(async position => {
+    const {latitude, longitude} = position.coords;
+
+    const div = document.createElement('div');
+    div.innerText = `Latitude: ${latitude} Longitude: ${longitude}`;
+    geoDiv.appendChild(div);
+
+    getWeather(latitude, longitude);
+  });
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
   document.getElementById('header').style.color = '#666';
   renderGoldPrices();
   initTranslations();
   displayNews();
+  initGeoWeather();
 });
