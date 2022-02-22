db9f2566bea9df34c1769cf287fd75373ec16c46
Adam <matusiak86.adam@gmail.com>
2022-02-22 10:15:01 +0100

    change button added

--- main.js
+++ main.js
@@ -16,29 +16,46 @@ generateCuriosity()
 
 // ------------------------------------------
 
-const currencyFirst = document.querySelector('#currency-one')
-const currencySecond = document.querySelector('#currency-two')
-const changeBtn = document.querySelector('.changing')
-const amountFirst = document.querySelector('base-amount')
-const amountSecond = document.querySelector('.converted-amount')
-const rateInfo = document.querySelector('.rate-info')
+// const currencyFirst = document.querySelector('#currency-one')
+// const currencySecond = document.querySelector('#currency-two')
+// const changeBtn = document.querySelector('.changing')
+// const amountFirst = document.querySelector('.base-amount')
+// const amountSecond = document.querySelector('.converted-amount')
+// const rateInfo = document.querySelector('.rate-info')
 
-const calculation = () => {
-    fetch(`https://v6.exchangerate-api.com/v6/89de10cd4b5fc62d16b4a029/pair/${currencyFirst.value}/${currencySecond.value}`)
-    .then(res => res.json())
-    .then(data => {
-        const currencyOne = currencyFirst.value
-        const currencyTwo = currencySecond.value
-        // console.log(data)
-        console.log(currencyOne);
-        console.log(currencyTwo);
+// const calculation = () => {
+//     fetch(`https://v6.exchangerate-api.com/v6/89de10cd4b5fc62d16b4a029/pair/${currencyFirst.value}/${currencySecond.value}`)
+//     .then(res => res.json())
+//     .then(data => {
+//         const currencyOne = currencyFirst.value
+//         const currencyTwo = currencySecond.value
+//         // console.log(data)
+//         // console.log(currencyOne)
+//         // console.log(currencyTwo)
 
-        const rate = data.conversion_rate[currencyTwo]       
-        console.log(rate);
-        rateInfo.textContent = `1 ${currencyOne} = ${rate} ${currencyTwo}`
+//         const rate = data.conversion_rate
+//         // console.log(rate)
+//         // let val = amountFirst.value
+                
+//         rateInfo.textContent = `1 ${currencyOne} to ${rate} ${currencyTwo}`
+//         // rateInfo.textContent = `${val}`
+
+//         amountSecond.value = (amountFirst.value * rate).toFixed(2)
         
-    })
-}
+//     })
+// }
 
-calculation ()
+// const change = () => {
+//     const firstValue = currencyFirst.value
+//     currencyFirst.value = currencySecond.value
+//     currencySecond.value = firstValue
+//     calculation ()
+// }
+
+// currencyFirst.addEventListener('change', calculation)
+// currencySecond.addEventListener('change', calculation)
+// amountFirst.addEventListener('input', calculation)
+// changeBtn.addEventListener('click', change)
+
+// calculation()
 
