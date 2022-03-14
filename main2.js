7bf2fa693604aa633e1a3e687eca973ada28b4d8
Adam <matusiak86.adam@gmail.com>
2022-03-14 15:34:37 +0100

    add color change, curiosity, new fonts

--- main2.js
+++ main2.js
@@ -11,6 +11,30 @@ const infoBtn = document.querySelector('.fa-circle-info');
 const explanation = document.querySelector('.explanation');
 const closeModalBtn = document.querySelector('.close');
 
+const colorBtn = document.querySelector('.fa-palette');
+const colorPanel = document.querySelector('.colors');
+const colorOne = document.querySelector('.first');
+const colorTwo = document.querySelector('.second');
+const colorThree = document.querySelector('.third');
+let root = document.documentElement;
+
+const currentDay = document.querySelector('.day')
+const curiosity = document.querySelector('.curiosity')
+
+const curiosityArr = ['W Singapurze zabronione jest żucie gumy.','Leonardo da Vinci potrafił malować jedną ręką i jednocześnie pisać drugą.', 'W trakcie oddychania żebra poruszają się rocznie 5 mln razy.', 'Przeciętny czterolatek zadaje ponad 400 pytań dziennie.', 'George Washington w swoim ogrodzie hodował konopie.', 'Od 1945 r. wszystkie brytyjskie czołgi wyposażone są w zestaw do parzenia herbaty.', 'Żeby wejść na wieżę Eiffla trzeba pokonać 1710 stopni.', 'W naszej galaktyce jest 100 miliardów gwiazd.', 'Chińczycy używają ponad 80 miliardów pałeczek w ciągu roku.', 'Aż pięć planet jesteśmy gotowi zobaczyć gołym okiem. Są to: Wenus, Merkury, Mars, Jowisz i Saturn..', 'Kiedyś litera Z została usunięta z alfabetu na aż 200 lat', 'Za niegrzeczne uważa się pisanie czerwonym tuszem w języku portugalskim.', 'Kopenhaga jest najbardziej przyjaznym rowerom miastem na świecie.', 'W 1889 roku królowa Włoch, Margherita Savoy, zamówiła pierwszą pizzę z dowozem.', 'Muhammad to najpopularniejsze imię na świecie.']
+
+const day = new Date()
+currentDay.textContent = day.toLocaleString("pl", {weekday:"long"})
+
+const generateCuriosity = () => {
+    const newCuriosity = Math.floor(Math.random() * curiosityArr.length)
+    // curiosity.innerHTML = `${curiosityArr[newCuriosity]}`
+    curiosity.textContent = curiosityArr[newCuriosity]
+}
+
+
+
+
 const calculation = () => {
     fetch(`https://v6.exchangerate-api.com/v6/89de10cd4b5fc62d16b4a029/pair/${currencyFirst.value}/${currencySecond.value}`)
     .then(res => res.json())
@@ -82,8 +106,33 @@ currencySecond.addEventListener('change', calculation)
 amountFirst.addEventListener('input', calculation)
 changeBtn.addEventListener('click', change)
 
-infoBtn.addEventListener('click', showExplanation);
-closeModalBtn.addEventListener('click', showExplanation);
-window.addEventListener('click', e => e.target === explanation ? showExplanation() : false);
+infoBtn.addEventListener('click', showExplanation)
+closeModalBtn.addEventListener('click', showExplanation)
+window.addEventListener('click', e => e.target === explanation ? showExplanation() : false)
 
+
+colorBtn.addEventListener('click', () => {
+    colorPanel.classList.toggle('show-colors')
+});
+
+colorOne.addEventListener('click', () => {
+    root.style.setProperty('--first-color', 'rgba(240, 14, 14, 0.562)');
+    // root.style.setProperty('--hover-color', 'rgb(201, 27, 85)');
+});
+
+colorTwo.addEventListener('click', () => {
+    root.style.setProperty('--first-color', 'rgba(255, 255, 255, 0.43)');
+    // root.style.setProperty('--hover-color', 'rgb(201, 27, 85)');
+});
+
+colorThree.addEventListener('click', () => {
+    root.style.setProperty('--first-color', 'rgba(27, 48, 182, 0.86)');
+    // root.style.setProperty('--hover-color', 'rgb(201, 27, 85)');
+});
+
+
+generateCuriosity()
 calculation()
+
+
+
