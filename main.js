2d7f42ab12b31aef2c41a30db5fecb9103af5d4b
elachcdv <elach@edu.cdv.pl>
2022-03-27 13:41:40 +0200

    Get movies, get suggested movies

--- main.js
+++ main.js
@@ -15,28 +15,27 @@ window.addEventListener("DOMContentLoaded", () => {
   const apiKey = "k_28x30ddn"; //    const apiKey= "k_pgd93xda"
   const inputMovieSearch = document.getElementById("searchMovie");
   const searchResultsMovies = document.getElementById("movieList");
+  const suggestedResultsList = document.getElementById("suggestedResultsList");
   const urlAPI = `https://imdb-api.com/en/API/SearchMovie/${apiKey}`;
   const urlDetailsAPI = `https://imdb-api.com/en/API/Title/${apiKey}`;
-
-  // function init() {
-  //   inputMovieSearch.addEventListener("keyup", (event) => {
-  //     // debounce(getMovies(event.target.value), 2000);
-  //     debounce(console.log(event.target.value), 2000);
-  //     //   console.log(event.target.value);
-  //     // movieValue(event);
-  //   });
+  const advancedSearchUrl = `https://imdb-api.com/API/AdvancedSearch/${apiKey}`;
 
   function init() {
     inputMovieSearch.addEventListener("keyup", debounce(getMovies, 300));
 
     searchResultsMovies.addEventListener("click", (event) => {
+      //zamienić na funkcję - powtarza sie
+      const movieID = event.target.closest("a").id;
+      getDetails(movieID);
+    });
+    suggestedResultsList.addEventListener("click", (event) => {
+      //zamienić na funkcję - powtarza sie powyżej
       const movieID = event.target.closest("a").id;
       getDetails(movieID);
     });
   }
   init();
 
-  //delay function for input search
   function debounce(callback, delay) {
     let timeout;
     return function () {
@@ -53,13 +52,13 @@ window.addEventListener("DOMContentLoaded", () => {
       .then((data) => data.results)
       .then((data) => {
         // console.log(data); // return results in console;
-        //  add to DOM
-        createMoviesDOM(data);
+        // add to DOM
+        createMoviesDOM(data, searchResultsMovies);
       })
       .catch((error) => console.log(error));
   }
 
-  function createMoviesDOM(movies) {
+  function createMoviesDOM(movies, htmlTarget) {
     for (const movie of movies) {
       const movieElement = document.createElement("div");
       movieElement.classList.add("singleMovieList");
@@ -67,7 +66,7 @@ window.addEventListener("DOMContentLoaded", () => {
       movieElement.innerHTML = `<a id="${movie.id}">${movie.title}</a>`;
       // movieElement.innerHTML = `<img src="${movie.image}" alt="${movie.title}">`;
       movieElement.style.backgroundImage = `url(${movie.image})`;
-      searchResultsMovies.appendChild(movieElement);
+      htmlTarget.appendChild(movieElement);
     }
   }
 
@@ -75,15 +74,36 @@ window.addEventListener("DOMContentLoaded", () => {
   async function getDetails(movieId) {
     const details = await fetch(`${urlDetailsAPI}/${movieId}`)
       .then((response) => response.json())
-      // .then((data) => data.detail)
       .then((data) => {
         // console.log(data);
         //add to DOM div after clicked on link
         createMovieDetailsDom(data);
+        const genres = transformGenresToApiParam(data.genreList); // " "
+        showSuggestions(genres);
       });
     // .catch((error) => new Error("Details not found"));
   }
 
+  // suggestions
+  async function showSuggestions(genres) {
+    // transformGenresToApiParam(genres);
+    const suggestions = await fetch(`${advancedSearchUrl}/?genres=${genres}`)
+      .then((response) => response.json())
+      .then((data) => data.results)
+      .then((data) => {
+        createMoviesDOM(data, suggestedResultsList);
+      });
+    // .catch((error) => new Error("Details not found"));
+  }
+
+  function transformGenresToApiParam(genres) {
+    const currentGenres = genres.map((genre) => {
+      return genre.value;
+    });
+    return currentGenres.join().toLowerCase(); // "genre1,genre2,genre3"
+  }
+
+  // function which create details DOM
   function createMovieDetailsDom(data) {
     const insideDetailBox = document.getElementById("currentClickedMovieId");
     insideDetailBox.innerHTML = "";
@@ -99,7 +119,6 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     //rightBox
-
     const detailBoxRight = document.createElement("div");
     detailBoxRight.classList.add("box-right");
     insideDetailBox.append(detailBoxRight);
@@ -140,7 +159,8 @@ window.addEventListener("DOMContentLoaded", () => {
     if (data.genres) {
       const detailBoxGenres = document.createElement("p");
       detailBoxGenres.innerHTML = `${data.genres}`;
-      detailBoxGenres.classList.add("year");
+      detailBoxGenres.classList.add("genres");
+      detailBoxGenres.id = "genres";
       detailBoxRight.appendChild(detailBoxGenres);
     }
 
@@ -154,7 +174,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     //stars - create ul
     if (data.starList) {
-      createStars(data.starList);
+      createStars(data.starList, detailBoxRight);
     }
 
     //runtimeStr
@@ -166,20 +186,15 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  //stars - create li inside ul
-  function createStars(stars) {
+  function createStars(stars, box) {
     const detailBoxStars = document.createElement("ul");
     detailBoxStars.classList.add("stars");
-    detailBoxRight.appendChild(detailBoxStars);
+    box.appendChild(detailBoxStars);
 
     for (const star of stars) {
       const starElement = document.createElement("li");
-      starElement.innerHTML = star.name; //`${Object.values(star)}`;
+      starElement.innerHTML = star.name;
       detailBoxStars.appendChild(starElement);
     }
   }
-
-  // Suggested movies by category
-  // search movies by genres
-  //
 });
