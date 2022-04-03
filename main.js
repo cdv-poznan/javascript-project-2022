f124504bb3370b375b31f3c422d7a3f279044e44
PeterM8811 <pmlynarski88@gmail.com>
2022-04-03 14:56:13 +0200

    JScode modernization #1

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 async function bookSearchByTitle() {
 
-    const request = fetch('http://openlibrary.org/search.json');
-    const search = (await request).json();
     const TextToSearch = document.getElementById('Text-to-search') 
     
     function getBookByTitleQuery(text) {
@@ -10,13 +8,30 @@ async function bookSearchByTitle() {
     
     const button = document.getElementById('Search');
     button.addEventListener('click', async ()=>{
+        document.getElementById("logo").style.display = "flex";
+        document.getElementById("logo").style.marginLeft = "0px";
+        document.getElementById("logo").style.marginRight = "0px";
+        document.getElementById("logo").style.height = "100px";
+        document.getElementById("logo").style.position = "absolute";
         const text = TextToSearch.value;
         const query = getBookByTitleQuery(text);
         const url = `http://openlibrary.org/search.json${query}`;
         const response = await fetch(url);
-        const {ResultOfSearch} = await response.json();
-        console.log({ResultOfSearch});
-
+        const {docs} = await response.json();
+        console.log({docs}); 
+        console.log(docs.length);
+        for (let i=0; i<docs.length; i++ ) {
+            const object = docs[i];
+            console.log(object);
+            const {key, title} = object;
+            console.log(title);
+            console.log('https://openlibrary.org'+ key);
+            let element = document.createElement('div');
+            let url = 'https://openlibrary.org'+ key
+            element.innerText = title + " " + url; 
+            element.classList.add('Result');
+            document.getElementById('Results').appendChild(element);
+        }
     })
 }
 
