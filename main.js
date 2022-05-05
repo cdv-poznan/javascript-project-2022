3973a4d6ad0af98c1a874f8ed2745d8ac3e33f14
MiloszStochaj <milosz.stochaj@gmail.com>
2022-05-05 21:34:20 +0200

    page navigation

--- main.js
+++ main.js
@@ -1,66 +1,101 @@
-const spellsInformationContainer = document.querySelector(".spells__container");
+const firstSpellsInformationContainer = document.querySelector(
+  ".spells__container--1"
+);
+const secondSpellsInformationContainer = document.querySelector(
+  ".spells__container--2"
+);
 const characterInformationContainer =
   document.querySelector(".character__data");
 const searchContainer = document.querySelector(".search__container");
-
+const showMoreBtn = document.querySelector('.show__more');
 
 // Page navigation
 
-// document.querySelectorAll('.nav__link').forEach(function (el) {
-//   el.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     const id = this.getAttribute('href');
-//     console.log(id);
-//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-//   });
-// });
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
 
-// 1. Add event listener to common parent element
-// 2. Determine what element originated the event
+  // Matching strategy
 
-document.querySelector('.nav__links').addEventListener('click', function (e) {
-    e.preventDefault();
-  
-    // Matching strategy
-    if (e.target.classList.contains('nav__link')) {
-      const id = e.target.getAttribute('href');
-      document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-    }
-  });
-
-
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
 
 window.addEventListener("DOMContentLoaded", (event) => {
   fetch(`https://fedeperin-harry-potter-api-en.herokuapp.com/spells`)
     .then((response) => response.json())
     .then((data) => {
-
-
       for (let i = 0; i < 72; i++) {
         const spellDataResult = data[i];
         console.log(spellDataResult);
-        
-    
-        const htmlSpells = `
+
+        if (i < 12) {
+          const firstHtmlSpells = `
         <div class='spells__information'>
   <p>${spellDataResult.spell}</p>
   <p class="use">${spellDataResult.use}</p>
 
 </div>`;
 
+          firstSpellsInformationContainer.insertAdjacentHTML(
+            "beforeend",
+            firstHtmlSpells
+          );
+        } else if (i >= 12) {
+          const secondHtmlSpells = `
+        <div class='spells__information'>
+  <p>${spellDataResult.spell}</p>
+  <p class="use">${spellDataResult.use}</p>
 
+</div>`;
 
-        spellsInformationContainer.insertAdjacentHTML("beforeend", htmlSpells);
+          secondSpellsInformationContainer.insertAdjacentHTML(
+            "beforeend",
+            secondHtmlSpells
+          );
         }
-
-       
+      }
     });
-    
 });
 
 
 
-// get information about Character
+
+// Show more spells button
+
+const showLess = function(){
+    secondSpellsInformationContainer.classList.add('hidden')
+        document.querySelector('.show__more').textContent = 'Show more'
+}
+
+const showMore = function(){
+    secondSpellsInformationContainer.classList.remove('hidden')
+    document.querySelector('.show__more').textContent = 'Show less'
+
+    showMoreBtn.addEventListener('click', showLess);
+}
+
+showMoreBtn.addEventListener('click', showMore);
+
+
+
+
+// showMoreBtn.addEventListener('click', function(){
+   
+
+//     secondSpellsInformationContainer.classList.remove('hidden')
+//     document.querySelector('.show__more').textContent = 'Show less'
+
+//     showMoreBtn.addEventListener('click', function(){
+//         secondSpellsInformationContainer.classList.add('hidden')
+//         document.querySelector('.show__more').textContent = 'Show more'
+//     })
+// });
+
+
+
+// Get information about Character
 
 const getInformationNick = function (characterNick) {
   fetch(`https://fedeperin-harry-potter-api-en.herokuapp.com/characters`)
@@ -100,13 +135,11 @@ function capitalizeTheFirstLetterOfEachWord(word) {
   return separateWord.join(" ");
 }
 
-
 // Action on click
 
 searchContainer.addEventListener("submit", function (e) {
   characterInformationContainer.innerHTML = "";
   e.preventDefault();
-  // const characterNameFromUser = document.querySelector('.input__form').value
   const characterNameFromUser = capitalizeTheFirstLetterOfEachWord(
     document.querySelector(".input__form").value
   );
