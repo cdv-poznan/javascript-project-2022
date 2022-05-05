50fc4e4214f068c3821599990f0cd18031fc4a69
MiloszStochaj <milosz.stochaj@gmail.com>
2022-05-05 14:18:46 +0200

    newAPI

--- main.js
+++ main.js
@@ -1,108 +1,115 @@
-const characterInformationContainer = document.querySelector('.character__data');
-const searchContainer = document.querySelector('.search__container');
+const spellsInformationContainer = document.querySelector(".spells__container");
+const characterInformationContainer =
+  document.querySelector(".character__data");
+const searchContainer = document.querySelector(".search__container");
 
 
+// Page navigation
 
-
-
-
-
-// document.getElementById('btn__form').addEventListener('click', function () {
-//     const inputForm = document.querySelector('.input__form').value;
-//     console.log(inputForm);
-//     getInformation(inputForm);
-
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
 // });
 
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
 
-
-/*
-const fillInformation = function(data){
-const html = `
-<div class="character__information">
-<h1>Character information:</h1>
-<p title="Name">❓${data.name}</p>
-<p title="Birth">👶🏻${data.dateOfBirth}</p>
-<p title="Gender">👥${data.gender}</p>
-<p title="Ancestry">🌀${data.ancestry}</p>
-<p title="House">🏠${data.house}</p>
-<p title="Wand">🪄${data.wand.core}</p>
-<p title="Patronus">🧙🏻‍♂️${data.patronus}</p>
-</div>
-<img class="character__image" src="${data.image}" alt="No_data">
-`;
-
-characterInformationContainer.insertAdjacentHTML('beforeend', html);
-
-};
-*/
-
-/*
-const request = fetch(`http://hp-api.herokuapp.com/api/characters`)
-.then(response => response.json())
-.then(data => fillInformation(data[1]));
-*/
-
-
-// get Information about character function
-
-const getInformation = function(characterName){
-fetch(`http://hp-api.herokuapp.com/api/characters`)
-.then(response => response.json())
-.then(data => {
-    const result = data.find( ({ name }) => name === `${characterName}` );
-    // console.log(result.name);
-    // console.log(data)
-
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+    e.preventDefault();
   
-        const html = `
-        <div class="character__information">
-        <h1>Character information:</h1>
-        <p title="Name">❓${result.name}</p>
-        <p title="Birth">👶🏻${result.dateOfBirth}</p>
-        <p title="Gender">👥${result.gender}</p>
-        <p title="Ancestry">🌀${result.ancestry}</p>
-        <p title="House">🏠${result.house}</p>
-        <p title="Wand">🪄${result.wand}</p>
-        <p title="Patronus">🧙🏻‍♂️${result.patronus}</p>
-        </div>
-        <img class="character__image" src="${result.image}" alt="No_data">
-        `;
-        
-        characterInformationContainer.insertAdjacentHTML('beforeend', html);
+    // Matching strategy
+    if (e.target.classList.contains('nav__link')) {
+      const id = e.target.getAttribute('href');
+      document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  fetch(`https://fedeperin-harry-potter-api-en.herokuapp.com/spells`)
+    .then((response) => response.json())
+    .then((data) => {
+
+
+      for (let i = 0; i < 72; i++) {
+        const spellDataResult = data[i];
+        console.log(spellDataResult);
         
     
+        const htmlSpells = `
+        <div class='spells__information'>
+  <p>${spellDataResult.spell}</p>
+  <p class="use">${spellDataResult.use}</p>
 
+</div>`;
+
+
+
+        spellsInformationContainer.insertAdjacentHTML("beforeend", htmlSpells);
+        }
+
+       
+    });
+    
 });
 
 
 
-    
+// get information about Character
+
+const getInformationNick = function (characterNick) {
+  fetch(`https://fedeperin-harry-potter-api-en.herokuapp.com/characters`)
+    .then((response) => response.json())
+    .then((data) => {
+      const nickDataResult = data.find(
+        ({ nickname }) => nickname === `${characterNick}`
+      );
+
+      console.log(nickDataResult);
+
+      const html = `
+        <img class="character__image" src="${nickDataResult.image}" alt="No_data">
+        <div class="character__information">
+        <h1>Character information:</h1>
+        <p title="Name">❓ ${nickDataResult.character}</p>
+        <p title="House">❓ ${nickDataResult.hogwartsHouse}</p>
+        <p title="Child">❓ ${nickDataResult.child}</p>
+        <p title="Actor">❓ ${nickDataResult.interpretedBy}</p>
+        
+        </div>
+        `;
+
+      characterInformationContainer.insertAdjacentHTML("beforeend", html);
+    });
 };
+getInformationNick("Harry");
 
 // Capitalize The First Letter Of Each Word in input__form
 
 function capitalizeTheFirstLetterOfEachWord(word) {
-    const separateWord = word.toLowerCase().split(' ');
-    for (let i = 0; i < separateWord.length; i++) {
-       separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
-       separateWord[i].substring(1);
-    }
-    return separateWord.join(' ');
- }
-//  console.log(capitalizeTheFirstLetterOfEachWord("harry potter"));
-
+  const separateWord = word.toLowerCase().split(" ");
+  for (let i = 0; i < separateWord.length; i++) {
+    separateWord[i] =
+      separateWord[i].charAt(0).toUpperCase() + separateWord[i].substring(1);
+  }
+  return separateWord.join(" ");
+}
 
 
 // Action on click
 
-searchContainer.addEventListener('submit', function (e) {
-    characterInformationContainer.innerHTML = '';
-    e.preventDefault();
-    // const characterNameFromUser = document.querySelector('.input__form').value
-    const characterNameFromUser = capitalizeTheFirstLetterOfEachWord(document.querySelector('.input__form').value);
-    getInformation(characterNameFromUser)
-    console.log(characterNameFromUser);
+searchContainer.addEventListener("submit", function (e) {
+  characterInformationContainer.innerHTML = "";
+  e.preventDefault();
+  // const characterNameFromUser = document.querySelector('.input__form').value
+  const characterNameFromUser = capitalizeTheFirstLetterOfEachWord(
+    document.querySelector(".input__form").value
+  );
+  getInformationNick(characterNameFromUser);
+  console.log(characterNameFromUser);
 });
-
-
