cb7ed92c19f869c0c2cacf8692e99251f051e7d2
Adam <matusiak86.adam@gmail.com>
2022-05-05 15:54:08 +0200

    responsive changes

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ const total = document.querySelector('.total');
 
 const infoBtn = document.querySelector('.fa-circle-info');
 const explanation = document.querySelector('.explanation');
-const save = document.querySelector('.fa-floppy-disk');
 const closeModalBtn = document.querySelector('.close');
 
 const colorBtn = document.querySelector('.fa-palette');
@@ -21,24 +20,22 @@ let root = document.documentElement;
 const currentDay = document.querySelector('.day');
 const curiosity = document.querySelector('.curiosity');
 
-
-
 const curiosityArr = [
-	'W Singapurze zabronione jest żucie gumy.',
-	'Leonardo da Vinci potrafił malować jedną ręką i jednocześnie pisać drugą.',
-	'W trakcie oddychania żebra poruszają się rocznie 5 mln razy.',
-	'Przeciętny czterolatek zadaje ponad 400 pytań dziennie.',
-	'George Washington w swoim ogrodzie hodował konopie.',
-	'Od 1945 r. wszystkie brytyjskie czołgi wyposażone są w zestaw do parzenia herbaty.',
-	'Żeby wejść na wieżę Eiffla trzeba pokonać 1710 stopni.',
-	'W naszej galaktyce jest 100 miliardów gwiazd.',
-	'Chińczycy używają ponad 80 miliardów pałeczek w ciągu roku.',
-	'Aż pięć planet jesteśmy gotowi zobaczyć gołym okiem. Są to: Wenus, Merkury, Mars, Jowisz i Saturn..',
-	'Kiedyś litera Z została usunięta z alfabetu na aż 200 lat',
-	'Za niegrzeczne uważa się pisanie czerwonym tuszem w języku portugalskim.',
-	'Kopenhaga jest najbardziej przyjaznym rowerom miastem na świecie.',
-	'W 1889 roku królowa Włoch, Margherita Savoy, zamówiła pierwszą pizzę z dowozem.',
-	'Muhammad to najpopularniejsze imię na świecie.',
+	'W Singapurze zabronione jest żucie gumy ?!',
+	'Leonardo da Vinci potrafił malować jedną ręką i jednocześnie pisać drugą ?!',
+	'W trakcie oddychania żebra poruszają się rocznie 5 mln razy ?!',
+	'Przeciętny czterolatek zadaje ponad 400 pytań dziennie ?!',
+	'George Washington w swoim ogrodzie hodował konopie ?!',
+	'Od 1945 r. wszystkie brytyjskie czołgi wyposażone są w zestaw do parzenia herbaty ?!',
+	'Żeby wejść na wieżę Eiffla trzeba pokonać 1710 stopni ?!',
+	'W naszej galaktyce jest 100 miliardów gwiazd ?!',
+	'Chińczycy używają ponad 80 miliardów pałeczek w ciągu roku ?!',
+	'Aż pięć planet jesteśmy gotowi zobaczyć gołym okiem. Są to: Wenus, Merkury, Mars, Jowisz i Saturn ?!',
+	'Kiedyś litera Z została usunięta z alfabetu na aż 200 lat ?!',
+	'Za niegrzeczne uważa się pisanie czerwonym tuszem w języku portugalskim ?!',
+	'Kopenhaga jest najbardziej przyjaznym rowerom miastem na świecie ?!',
+	'W 1889 roku królowa Włoch, Margherita Savoy, zamówiła pierwszą pizzę z dowozem ?!',
+	'Muhammad to najpopularniejsze imię na świecie ?!',
 ];
 
 const day = new Date();
@@ -64,8 +61,8 @@ const calculation = () => {
 			amountSecond.value = (amountFirst.value * rate).toFixed(2);
 			if (amountFirst.value > '0') {
 				total.textContent = `${amountFirst.value} ${currencyOne} to ${amountSecond.value} ${currencyTwo}`;
-				clear()
-			}
+			} else if (amountFirst.value === '' || amountFirst.value === "" || amountFirst.value === undefined) {
+                total.textContent = ``; }
 		});
 };
 
@@ -82,13 +79,8 @@ const showExplanation = () => {
 	} else {
 		explanation.style.display = 'none';
 	}
-
 	explanation.classList.toggle('modal-animation');
 };
-
-const saveConversion = () => {
-	
-}
 
 currencyFirst.addEventListener('change', calculation);
 currencySecond.addEventListener('change', calculation);
@@ -97,7 +89,6 @@ changeBtn.addEventListener('click', change);
 
 infoBtn.addEventListener('click', showExplanation);
 closeModalBtn.addEventListener('click', showExplanation);
-saveBtn.addEventListener('click', saveConversion )
 
 window.addEventListener('click', (e) =>
 	e.target === explanation ? showExplanation() : false
