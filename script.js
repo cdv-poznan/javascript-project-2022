805b8895c75933b933a4f039d47e51fcd70c566f
martabakaj <marta.bakaj@gmail.com>
2022-05-19 21:20:30 +0200

    Working AURORA DATA on map

--- script.js
+++ script.js
@@ -1,42 +1,61 @@
 function initMap() {
-    const myLatlng = { lat: 52.04789641, lng: 16.93403582 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: myLatlng,
-    });
-    // Create the initial InfoWindow.
-    let infoWindow = new google.maps.InfoWindow({
-      content: "Click the map to get Lat/Lng!",
-      position: myLatlng,
-    });
-  
-    infoWindow.open(map);
-    // Configure the click listener.
-    map.addListener("click", (mapsMouseEvent) => {
-      // Close the current InfoWindow.
-      infoWindow.close();
-      // Create a new InfoWindow.
-      infoWindow = new google.maps.InfoWindow({
-        position: mapsMouseEvent.latLng,
-      });
-      infoWindow.setContent(
-        JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-      );
-      infoWindow.open(map);
-           
- // API URL AURORA
- const apiURL="https://api.auroras.live/v1/?type=all&lat=40.7813913&long=-73.976902&forecast=false&threeday=false";
-var apiURLlink ="https://api.auroras.live/v1/?type=all&" + "lat="+mapsMouseEvent.latLng.toJSON().lat+"&long="
-+mapsMouseEvent.latLng.toJSON().lng + "&forecast=false&threeday=false"
- function getApiResponse(url) {
-   const postsRequest = fetch(url);
-   return postsRequest.then((response) => response.json()
-   );
- }
- 
- getApiResponse(apiURL).then((auroraData) => {
-   console.log(auroraData);
-    });
-    });
-  }
+  const myLatlng = { lat: 52.41573519438039, lng: 16.93194798098 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 5,
+    center: myLatlng,
+  });
+  // Create the initial InfoWindow.
+  let infoWindow = new google.maps.InfoWindow({
+    content: "Hello in Aurora project!",
+    position: myLatlng,
+  });
+  let infoWindow2 = new google.maps.InfoWindow({});
 
+  infoWindow.open(map);
+
+  // Configure the click listener.
+  map.addListener("click", (mapsMouseEvent) => {
+    // Close the current InfoWindow.
+    infoWindow.close();
+    // Create a new InfoWindow.
+    infoWindow = new google.maps.InfoWindow({
+      position: mapsMouseEvent.latLng,
+    });
+    infoWindow.setContent("loading...");
+    infoWindow.open(map);
+
+    // API URL AURORA
+    var apiURLlink =
+      "https://api.auroras.live/v1/?type=all&" +
+      "lat=" +
+      mapsMouseEvent.latLng.toJSON().lat +
+      "&long=" +
+      mapsMouseEvent.latLng.toJSON().lng +
+      "&forecast=false&threeday=false&colour=hex";
+
+    function getApiResponse(url) {
+      const postsRequest = fetch(url);
+      return postsRequest.then((response) => response.json());
+    }
+
+    getApiResponse(apiURLlink).then((auroraData) => {
+      infoWindow.setContent("ACTUAL KP: " + auroraData.ace.kp);
+      infoWindow2.close();
+      infoWindow2 = new google.maps.InfoWindow({
+        position: {
+          lat: parseFloat(auroraData.probability.highest.lat),
+          lng: parseFloat(auroraData.probability.highest.long),
+        },
+      });
+
+      infoWindow2.setContent(
+        "The bigest probability of Aurora is here: " +
+          auroraData.probability.highest.value +
+          "%"
+      );
+      infoWindow2.open(map);
+
+      console.log(auroraData);
+    });
+  });
+}
