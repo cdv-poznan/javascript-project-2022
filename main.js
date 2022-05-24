beb84b1d89822079c6b9cabd078ae09c205fb6bd
zwi3rzak <frontmatend@gmail.com>
2022-05-25 00:32:58 +0200

    fetch - Contact List Flex Wrap - reqres.in

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 fetch("https://reqres.in/api/users/")
   .then((response) => response.json())
   .then((json) => {
-    console.log(json);
+    console.log(json.data);
+    const html = json.data.map((user) => {
+      return `
+        <div class="contact">
+                <img class="image" src="${user.avatar}">
+            <div class="name">
+                <span class="id">${user.id}</span>
+                <span class="firstName">${user.first_name}</span>
+                <span class="lastName">${user.last_name}</span>
+            </div>
+            <p class="email">${user.email}</p>
+         </div>
+        `;
+    });
+    document.querySelector('.list').innerHTML = html.join('');
   });
