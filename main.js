9a819f93dc641114f88561d4e4e72670fa0120fd
arozynek <panna.anna.r@gmail.com>
2022-05-24 23:05:26 +0200

    Colors generator is working

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
-
 document.addEventListener("DOMContentLoaded", () => {
+  const api = "https://x-colors.herokuapp.com/api/random";
 
+  // -------------------------CANVAS-----------------------
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext("2d");
+  // Resizing
+  canvas.height = window.innerHeight / 1.5;
+  canvas.width = window.innerWidth;
+  window.addEventListener("resize", () => {
+    canvas.height = window.innerHeight / 1.5;
+    canvas.width = window.innerWidth / 1.1;
+  });
+  // Variables
+  let painting = false;
 
-// Resizing
-canvas.height = window.innerHeight/ 1.5;
-canvas.width = window.innerWidth/1.1;
-window.addEventListener("resize", () => {
-    canvas.height = window.innerHeight/ 1.5;
-    canvas.width = window.innerWidth/1.1;
-})
-// Variables
-let painting = false;
-
-const startPosition = function () {
+  const startPosition = function () {
     painting = true;
-};
-const finishPosition = function () {
+  };
+  const finishPosition = function () {
     painting = false;
     ctx.beginPath();
-};
+  };
 
-// Canvas position
-function getMousePos(canvas, e) {
+  // Canvas position
+  function getMousePos(canvas, e) {
     var rect = canvas.getBoundingClientRect();
     // console.log(rect);
     return {
-        x: (e.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
-        y: (e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+      x: ((e.clientX - rect.left) / (rect.right - rect.left)) * canvas.width,
+      y: ((e.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height,
     };
-}
+  }
 
-
-function draw (e) {
+  function draw(e) {
     if (!painting) return;
     ctx.lineWidth = 10;
     ctx.lineCap = "round";
@@ -43,22 +42,51 @@ function draw (e) {
     ctx.stroke();
     // ctx.beginPath();
     // ctx.moveTo(mousePosition.clientX, mousePosition.clientY);
-};
+  }
 
-const navWidth = document.getElementById("nav");
-console.log(navWidth.style.width);
-// Event listeners
-canvas.addEventListener("mousedown", startPosition);
-canvas.addEventListener("mouseup", finishPosition);
-canvas.addEventListener("mousemove", draw);
-// Clearing canvas
-const clearBttn = document.getElementById("clear-button");
-clearBttn.addEventListener("click", function() { ctx.clearRect(0, 0, canvas.width, canvas.height) })
+  // Event listeners
+  canvas.addEventListener("mousedown", startPosition);
+  canvas.addEventListener("mouseup", finishPosition);
+  canvas.addEventListener("mousemove", draw);
+  // Clearing canvas
+  const clearBttn = document.getElementById("clear-button");
+  clearBttn.addEventListener("click", function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  });
+  // ------------------------CANVAS-END-----------------------------------------
 
+  // Generating colors
+  function getApiResponse(url) {
+    const colorsRequest = fetch(url);
+    return colorsRequest
+      .then((response) => response.json())
+      .catch((error) => {
+        alert("Error - data can't be loaded");
+      });
+  }
+  const colorButton = document.getElementById("color-button");
+  colorButton.addEventListener("click", getColors);
+  function getColors() {
+    let oldColors = document.getElementsByClassName('new-colors');
+    for  (var i = (oldColors.length - 1) ; i >= 0; i--) {
+    oldColors[i].parentNode.removeChild(oldColors[i])
+    };
+    getApiResponse(api).then((colors) => addColorStyle(colors.hex));
+    getApiResponse(api).then((colors) => addColorStyle(colors.hex));
+    getApiResponse(api).then((colors) => addColorStyle(colors.hex));
+  };
+{/* <span class="material-symbols-outlined">
+circle
+</span> */}
+  function addColorStyle(hex) {
+    const colors = document.getElementById("generated-colors");
+    const colorsElement = document.createElement("span");
+    colorsElement.style.color = hex;
+    colorsElement.classList = 'new-colors'
+    colorsElement.innerText = "Color";
+    colors.append(colorsElement);
+  }
 
-
-// //whole canvas
-// ctx.fillStyle = 'white';
-
-
-})
+  // //whole canvas
+  // ctx.fillStyle = 'white';
+});
