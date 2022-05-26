4d68bfbf894264c41751da6864d78df9b1f9efc6
arozynek <panna.anna.r@gmail.com>
2022-05-26 20:59:39 +0200

    Adding emojis on canvas enabled

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const api = "https://x-colors.herokuapp.com/api/random";
+  const localApi = "http://localhost:5000/api/random";
 
-$(document).ready(function(){
-  $('#new-image').click(function() {
-    $('#header').slideToggle()
-  })
-});
-
-
-
+  $(document).ready(function () {
+    $("#new-image").click(function () {
+      $("#header").slideToggle().css("display", "flex");
+    });
+  });
 
   // -------------------------CANVAS-----------------------
   const canvas = document.getElementById("canvas");
@@ -19,8 +17,20 @@ $(document).ready(function(){
   canvas.width = window.innerWidth;
   window.addEventListener("resize", () => {
     canvas.height = window.innerHeight / 1.5;
-    canvas.width = window.innerWidth / 1.1;
+    canvas.width = window.innerWidth;
   });
+
+  // Canvas position
+  function getMousePos(canvas, e) {
+    var rect = canvas.getBoundingClientRect();
+    // console.log(rect);
+    return {
+      x: ((e.clientX - rect.left) / (rect.right - rect.left)) * canvas.width,
+      y: ((e.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height,
+    };
+  }
+
+
   // Variables
   let painting = false;
 
@@ -32,15 +42,6 @@ $(document).ready(function(){
     ctx.beginPath();
   };
 
-  // Canvas position
-  function getMousePos(canvas, e) {
-    var rect = canvas.getBoundingClientRect();
-    // console.log(rect);
-    return {
-      x: ((e.clientX - rect.left) / (rect.right - rect.left)) * canvas.width,
-      y: ((e.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height,
-    };
-  }
 
   function draw(e) {
     if (!painting) return;
@@ -64,23 +65,22 @@ $(document).ready(function(){
   });
   // ------------------------CANVAS-END-----------------------------------------
 
- function getApiResponse(url) {
+  function getApiResponse(url) {
     const apiRequest = fetch(url);
-    return apiResponse = apiRequest
-    .then((response) => response.json())
-    .catch((error) => {
-      alert(
-        `Error - data can't be loaded from:
-        ${url}`);
-      });
+    return (apiResponse = apiRequest
+      .then((response) => response.json())
+      .catch((error) => {
+        alert(
+          `Error - data can't be loaded from:
+        ${url}`
+        );
+      }));
 
-
-  // Generating colors
+    // Generating colors
   }
   const colorButton = document.getElementById("color-button");
   colorButton.addEventListener("click", () => {
     getColors();
-    
   });
 
   function getColors() {
@@ -88,13 +88,19 @@ $(document).ready(function(){
     for (var i = oldColors.length - 1; i >= 0; i--) {
       oldColors[i].parentNode.removeChild(oldColors[i]);
     }
-    getApiResponse(api).then((colors) => addColorStyle(colors.hex, "color1"));
-    getApiResponse(api).then((colors) => addColorStyle(colors.hex, "color2"));
-    getApiResponse(api).then((colors) => addColorStyle(colors.hex, "color3"));
+    getApiResponse(localApi).then((colors) =>
+      addColorStyle(colors.hex, "color1")
+    );
+    getApiResponse(localApi).then((colors) =>
+      addColorStyle(colors.hex, "color2")
+    );
+    getApiResponse(localApi).then((colors) =>
+      addColorStyle(colors.hex, "color3")
+    );
   }
 
   function addColorStyle(hex, i) {
-/*    ------------------- XML file --------------------------
+    /*    ------------------- XML file --------------------------
 //  when first colors API doesn't work
 
     var url = "http://colormind.io/api/";
@@ -129,71 +135,74 @@ http.send(JSON.stringify(data));
     colorsElement.classList = "material-symbols-rounded";
     colorsElement.id = i;
     colorsElement.innerText = "circle";
-    colorsElement.addEventListener('click', ()=> {
-        // console.log('clicked');
-        ctx.strokeStyle = `${hex}`;
-    })
+    colorsElement.addEventListener("click", () => {
+      // console.log('clicked');
+      ctx.strokeStyle = `${hex}`;
+    });
     colors.append(colorsElement);
-}
+  }
 
-// ------------------------emojis-----------------
-const apiKey = 'b2d5f5b8a462e384858bbfeaf3f0bf1b84fcd82e';
-const emojiList = `https://emoji-api.com/emojis?access_key=${apiKey}`;
+  // ------------------------emojis-----------------
+  const apiKey = "b2d5f5b8a462e384858bbfeaf3f0bf1b84fcd82e";
+  const emojiList = `https://emoji-api.com/emojis?access_key=${apiKey}`;
 
-class Emoji {
-constructor(group, character, id) {
-  this.id = id;
-  this.group = group;
-  this.character = character;
-};
-renderGroup(){
-  const group = document.getElementById("generated-emojis");
-  const groupElement = document.createElement("span");
-  groupElement.innerText = this.group;
-  groupElement.addEventListener('click', () => {
-    console.log('clicked group')
-  })
-  group.append(groupElement);
-};
-renderEmoji(){
-  const emojis = document.getElementById("generated-emojis");
-  const emojisElement = document.createElement("span");
-  emojisElement.innerText = this.character;
-  emojisElement.addEventListener('click', () => {
-    console.log('clicked emoji')
-  })
-  emojis.append(emojisElement);
-};
-}
+  class Emoji {
+    constructor(group, character, id) {
+      this.id = id;
+      this.group = group;
+      this.character = character;
+    }
+    renderGroup() {
+      const group = document.getElementById("generated-emojis");
+      const groupElement = document.createElement("span");
+      groupElement.innerText = this.group;
+      groupElement.addEventListener("click", () => {
+        console.log("clicked group");
+      });
+      group.append(groupElement);
+    }
+    renderEmoji() {
+      const emojis = document.getElementById("generated-emojis");
+      const emojisElement = document.createElement("span");
+      emojisElement.innerText = this.character;
+      emojisElement.addEventListener("click", () => {
+        ctx.font = '15vh verdana';
+        // use these alignment properties for "better" positioning
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        painting = false;
+        // draw the emoji
+        canvas.addEventListener('click', (e) =>{
+        var mousePos = getMousePos(canvas, e);
+        ctx.fillText(this.character, mousePos.x, mousePos.y);
+      }, {once: true})
+      });
+      emojis.append(emojisElement);
+    }
+  }
 
-const emojiButton = document.getElementById("emojis-button");
-emojiButton.addEventListener("click", async () => {
-  const emojiJson = await getApiResponse(emojiList);
-  let counter = 0
-  
-  for (let i = counter ; i< counter + 10; i++) {
-    const emoji = new Emoji(emojiJson[i].group , emojiJson[i].character, i);
-    emoji.renderEmoji();
-    const nextButton = document.getElementById('next');
-    nextButton.addEventListener('click', () => {
-      counter += 1;
-      
-    });
-    const previousButton = document.getElementById('previous');
-    previousButton.addEventListener('click', () => {
-      counter -= 1;
-      
-    });
-  };
+  const emojiButton = document.getElementById("emojis-button");
+  emojiButton.addEventListener("click", async () => {
+    const emojiJson = await getApiResponse(emojiList);
+    let counter = 0;
+
+    for (let i = counter; i < counter + 10; i++) {
+      const emoji = new Emoji(emojiJson[i].group, emojiJson[i].character, i);
+      emoji.renderEmoji();
+      const nextButton = document.getElementById("next");
+      nextButton.addEventListener("click", () => {
+        counter += 1;
+      });
+      const previousButton = document.getElementById("previous");
+      previousButton.addEventListener("click", () => {
+        counter -= 1;
+      });
+    }
+  });
+
+  // let emojis = [];
+  // getApiResponse(emojiList).then(value => console.log(value, typeof value));
+  // getApiResponse(emojiList).then(value => (value.map (element => emojis.push(element.character))));
+
+  // end of DOMContentLoaded
 });
-
-// let emojis = [];
-// getApiResponse(emojiList).then(value => console.log(value, typeof value));
-// getApiResponse(emojiList).then(value => (value.map (element => emojis.push(element.character))));
-
-
-// end of DOMContentLoaded
-});
-
-
-
