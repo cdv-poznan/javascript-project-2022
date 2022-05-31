f224fa91352ceb84a924fd5c22ed78f046edbcef
OlgaRakavets <olgarakowec@gmail.com>
2022-05-31 19:27:27 +0200

    Przesunięcie losowania do zewnętrznej funkcji, dodano wywołanie Losuj po załadowaniu strony

--- main.js
+++ main.js
@@ -3,18 +3,25 @@ const $random_button = document.getElementById("random_button");
 
 const $Youtube_link = document.getElementById("Youtube_link");
 
-$random_button.addEventListener("click", () => {
-    for(let i=0;i<3;i++){
-  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res);
+function Losuj() {
+  for(let i=0;i<3;i++){
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+  
+        const strSource = res.meals[0].strMealThumb;
+        const newTitle = res.meals[0].strMeal;
+        const Source =res.meals[0].strSource
+        const Youtube = res.meals[0].strYoutube
+        document.getElementsByClassName("new_title")[i].innerText = newTitle;
+        document.getElementsByClassName("img_random")[i].src = strSource;
+        document.getElementsByClassName("Source")[i].href = Source
+        document.getElementsByClassName("Youtube_link")[i].href=Youtube
+      });
+      }
+}
 
-      const strSource = res.meals[0].strMealThumb;
-      const newTitle = res.meals[0].strMeal;
-      document.getElementsByClassName("new_title")[i].innerText = newTitle;
-      document.getElementsByClassName("img_random")[i].src = strSource;
-      
-    });
-    }
+$random_button.addEventListener("click", () => {
+    Losuj()
 });
