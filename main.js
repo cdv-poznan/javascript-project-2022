e467a64cf6dff198fbd4eca18227452f3c301b94
kasiasolinska <catherine.bus@gmail.com>
2022-06-30 07:36:58 +0200

    aplikacja pogodowa

--- main.js
+++ main.js
@@ -1,47 +1,22 @@
-const form = document.getElementById("form");
-const search = document.getElementById("search");
-const result = document.getElementById("result");
+const apiKey = "0bd56a60b494c2b8ef7759890948e62e";
+const apiUrl = "http://api.weatherstack.com/";
 
-// api url
-const api_url = "https://www.chartlyrics.com/api.aspx"; //nie działa
-//const api_url = "https://api.lyrics.ovh"; //nie działa
+const form = document.querySelector("form");
+const weatherDetails = document.querySelector(".details");
 
-// Defining async function
-async function getapi(url) {
-  // Storing response
-  const response = await fetch("${api_url}");
+//event listener
 
-  // Storing data in form of JSON
-  const data = await response.json();
-  console.log(data);
-  if (response) {
-    hideloader();
-  }
-  showData(data);
-}
-// Calling that async function
-//getapi(url);
-
-// Function to hide the loader
-function hideloader() {
-  document.getElementById("loading").style.display = "none";
-}
-
-function showData(data) {
-  result.innerHTML = `<ul>${data.data
-    .map((song) => `<li></li>`)
-    .join("")}</ul>`;
-}
-//  event listener
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  const location = document.querySelector("input").value;
+  getWeather(location);
 });
 
-//alert
-const searchTerm = search.value.trim();
-
-if (!searchTerm) {
-  alert("Type in a search term");
-} else {
-  searchSongs(searchTerm);
+async function getWeather(location) {
+  const response = await fetch(
+    `${apiUrl}current?access-key=${apiKey}&query=${location}`
+  );
+  const data = await response.json();
+  console.log(data);
 }
